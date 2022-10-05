7732ec51454e4015fbdfc756c0b96b37d26f2046
Al2003x <Danilovvik@bk.ru>
2022-10-05 23:21:36 +0300

    init project

---
+++ src/components/app/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import PageTitle from "../page-title/page-title";
+
+export default function App() {
+  return (
+    <>
+    <PageTitle title="Weather App" />
+    </>
+  );
+}
---
+++ src/components/page-title/page-title.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function PageTitle({ title }) {
+  return <h1 className="page__title">{title}</h1>;
+}
